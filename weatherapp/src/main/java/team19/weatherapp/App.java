8cfafdd90b5854887db0eb511d0f7b03a9799e9b
Scott Mackie <mrscottmackie@gmail.com>
2015-02-09 00:20:13 -0500

    Test Commit

---
+++ weatherapp/src/main/java/team19/weatherapp/App.java
@@ -0,0 +1,13 @@
+package team19.weatherapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
