3e29fe7a17edbafa3505b083fabdd82fe02d0058
Scott <mrscottmackie@gmail.com>
2015-03-08 16:26:11 -0400

    Stage 2 Update

--- weatherapp/src/main/java/team19/weatherapp/App.java
+++ weatherapp/src/main/java/team19/weatherapp/App.java
@@ -1,25 +1,52 @@
 package team19.weatherapp;
 
 import java.io.IOException;
-
 import javax.swing.SwingUtilities;
 
-//Initialize App
+/**
+ * App is the main class that will be loaded. 
+ *
+ * First, it initializes the SwingUtilities runnable, 
+ * and creates a null window.
+ * 
+ * Next, It creates a MainFrame object, which initializes 
+ * the gui for the application.
+ * 
+ * After intialization, we adjust and resize the window.
+ * 
+ * @author Scott Mackie
+ * @version     %I%, %G%
+ * 
+ */
 public class App {
 	
-	//Main Method
+	/**
+	 * 
+	 * Main method is initialized at startup.
+	 * 
+	 * It initializes the runnable, and runs until the
+	 * application is exited by the user.
+	 * 
+	 * After window creation, the main method handles window 
+	 * preferences (resize, location, etc.)
+	 * 
+	 * @param args  No arguments are needed to run the application
+	 * @throws IOException    An IOException can be thrown if the MainFrame
+	 * 						  throws an error during creation
+	 */
 	public static void main(String[] args) throws IOException {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				
-				//Display window, set size and properties
+				//Initialize MainWindow object
 				MainFrame window = null;
 				try {
 					window = new MainFrame();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				
+				//Set window preferences
 				window.setSize(465, 815);
 				window.setVisible(true);
 				window.setLocationRelativeTo(null);
--- weatherapp/src/main/java/team19/weatherapp/LongTermForecast.java
+++ weatherapp/src/main/java/team19/weatherapp/LongTermForecast.java
@@ -2,6 +2,21 @@ package team19.weatherapp;
 
 import org.json.JSONObject;
 
+
+/**
+ * The LongTermForecast class holds all of the long term weather data
+ * for the specified city. When the json is parsed, it is passed into
+ * the LongTermForecast constructor, which retrieves the data from the
+ * JSONObject and saves it into multiple variables.
+ * 
+ * Once saved, a user can easy access any of these variables by
+ * referencing them through the city object.
+ * 
+ * Currently, this is a placeholder for future development needs.
+ * 
+ * @author Scott Mackie
+ *
+ */
 public class LongTermForecast {
 
 	public LongTermForecast(JSONObject j) {
