e55f6a1acfa0e05fc33f8e39ee821392f6f13603
Jannie On <jon4@uwo.ca>
2015-03-19 19:06:16 -0400

    Added Andy's Locations + LocationSearchTree classes

--- weatherapp/src/main/java/team19/weatherapp/Location.java
+++ weatherapp/src/main/java/team19/weatherapp/Location.java
@@ -1,5 +1,13 @@
 package team19.weatherapp;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+
 /**
  * The Location class allows us to look up specific location from a data set.
  * 
@@ -11,7 +19,70 @@ package team19.weatherapp;
 public class Location {
 	
 	String[] cityList;
+	int locId;
+	String name;
+	String countryCode;
+	Location leftChild;
+	Location rightChild;
+	Location parent;
 
+	public Location (int id, String name, String countryCode) {
+		this.locId = id;
+		this.name = name;
+		this.countryCode = countryCode;
+		this.parent = null;
+		this.leftChild = null;
+		this.rightChild = null;
+	}
+	
+	public static Location makeLocation(String line) {
+		StringTokenizer st;
+		int id;
+		String name, countryCode, temp;
+		st = new StringTokenizer(line);
+        id = Integer.parseInt(st.nextToken());
+        name = st.nextToken();
+        temp = st.nextToken();
+        while (!((temp.charAt(0) > 47 && temp.charAt(0) < 58) || temp.charAt(0) == 45)) {
+     	   name = name.concat(" " + temp);
+     	   temp = st.nextToken();
+        }
+        st.nextToken();
+        countryCode = st.nextToken();
+        Location locObject = new Location(id, name, countryCode);
+        return locObject;
+        
+	}
+	
+	public static void buildTree (String cityListName) throws IOException, ClassNotFoundException{
+		FileReader file = new FileReader(cityListName);
+		BufferedReader br = new BufferedReader(file);
+		Location temp, root;
+		int i = 0;
+		String line, tempName;
+		br.readLine();
+		line = br.readLine();
+		root = makeLocation(line);
+		LocationSearchTree tree = new LocationSearchTree(root);
+        while ((line = br.readLine()) != null) {
+        	i++;
+        	temp = makeLocation(line);
+        	tempName = temp.getName();
+        	//System.out.println(tempName);
+        	tree.insert(temp);
+        }  
+		System.out.println(i);
+		//tree.traverseTree(root);
+		int [] ids = new int[10];
+		tree.findCityList("Jasp", root, ids);
+		ids = new int[10];
+		tree.findCityList("Tor", root , ids);
+		ids = new int[10];
+		tree.findCityList("Ra", root, ids);
+		br.close();
+		file.close();
+	}
+	
 	/**
 	 * The search method will allow us to search for specific cities.
 	 * 
@@ -20,6 +91,43 @@ public class Location {
 	 */
 	public static String search(String s) {
 		return "";
+	}
+	
+	public int getId() {
+		return this.locId;
+	}
+	
+	public String getName() {
+		return this.name + " " + this.countryCode;
+	}
+	
+	public void setLeft(Location newLeft) {
+		this.leftChild = newLeft;
+	}
+	
+	public Location getLeft() {
+		return this.leftChild;
+	}
+	
+	public void setRight(Location newRight) {
+		this.rightChild = newRight;
+	}
+	
+	public Location getRight() {
+		return this.rightChild;
+	}
+	
+	public void setParent(Location newParent) {
+		this.parent = newParent;
+	}
+	
+	public Location getParent() {
+		return this.parent;
+	}
+	
+	public String toString() {
+		String locInfo = this.locId + " " + this.name + ", " + this.countryCode;
+		return locInfo;
 	}
 
 }
