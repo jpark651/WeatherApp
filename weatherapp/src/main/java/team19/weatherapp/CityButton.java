3642430f823ae36d5fd3f67d76223d2f7b05d05b
Scott <mrscottmackie@gmail.com>
2015-03-29 20:21:18 -0400

    updated color buttons

--- weatherapp/src/main/java/team19/weatherapp/CityButton.java
+++ weatherapp/src/main/java/team19/weatherapp/CityButton.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 
 public class CityButton extends JButton{
 private CityButton currentbutton;
@@ -13,6 +14,10 @@ private CityButtonPanel cbp;
 String cityid;
 
 	public CityButton(final String c, CityButtonPanel cp, String ctid){
+		this.setOpaque(true);
+		this.setBackground(new Color(240,240,240));
+
+
 		this.setText(c);
 		this.currentbutton = this;
 		this.cbp = cp;
@@ -22,6 +27,7 @@ String cityid;
 		public void actionPerformed(ActionEvent event) {
 			Component[] cb = cbp.getComponents();
 			for(int i=0;i<cb.length;i++){
+				((JComponent) cb[i]).setOpaque(true);
 				cb[i].setBackground(new Color(240,240,240));
 			}
 			currentbutton.setBackground(new Color(240,150,150));
